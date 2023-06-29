8ce81d23c5e41ce4fd25f269c43c6284c00e6686
Serge Huber <shuber@jahia.com>
2023-06-29 11:01:30 +0200

    - Upgrade all dependencies to the latest versions, including upgrading Webpack 4 to 5 - Add NodeJS and Yarn version detection - Added more resources to watch configuration - Removed committed plugin with library - Added a test script for easy testing of the script (yarn test) - Upgraded ESLint and general code cleanup

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 #!/usr/bin/env node
 
-// Usage: npx @jahia/create-jahia-templateset@latest module-name 
+// Usage: npx @jahia/create-jahia-templateset@latest module-name
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import replace from 'replace-in-file';
-import camelCase from 'camelcase';
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import replace from 'replace-in-file'
+import camelCase from 'camelcase'
+import semver from 'semver'
+import { execSync } from 'child_process'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // The first argument will be the project name.
 if (process.argv.length < 3) {
-  console.error('Missing module-name parameter. Ex: npx @jahia/create-jahia-templateset@latest module-name');
-  process.exit(9);
+  console.error('Missing module-name parameter. Ex: npx @jahia/create-jahia-templateset@latest module-name')
+  process.exit(9)
 }
-const projectName = process.argv[2];
-const camelProjectName = camelCase(projectName);
+const projectName = process.argv[2]
+const camelProjectName = camelCase(projectName)
+
+let yarnPackageName = 'package.tgz'
+
+// First let's do some version checks
+console.log('Node version detected:', process.versions.node)
+const yarnVersion = execSync('yarn --version', { encoding: 'utf8' })
+console.log('Yarn version:', yarnVersion)
+if (semver.satisfies(yarnVersion, '1.x')) {
+  console.log('Yarn classic detected')
+  yarnPackageName = '$$$$MODULE_NAME$$$$-v1.0.0.tgz'
+}
 
 // Create a project directory with the project name.
-const currentDir = process.cwd();
-const projectDir = path.resolve(currentDir, projectName);
-fs.mkdirSync(projectDir, { recursive: true });
+const currentDir = process.cwd()
+const projectDir = path.resolve(currentDir, projectName)
+fs.mkdirSync(projectDir, { recursive: true })
 
 // A common approach to building a starter template is to
 // create a `template` folder which will house the template
 // and the files we want to create.
-const templateDir = path.resolve(__dirname, 'template');
-fs.cpSync(templateDir, projectDir, { recursive: true });
+const templateDir = path.resolve(__dirname, 'template')
+fs.cpSync(templateDir, projectDir, { recursive: true })
 
 // It is good practice to have dotfiles stored in the
 // template without the dot (so they do not get picked
@@ -38,42 +51,47 @@ fs.cpSync(templateDir, projectDir, { recursive: true });
 fs.renameSync(
   path.join(projectDir, 'dotgitignore'),
   path.join(projectDir, '.gitignore')
-);
+)
 fs.renameSync(
   path.join(projectDir, 'dotenv'),
   path.join(projectDir, '.env')
-);
+)
 
 // rename the resource file to use the project name
 fs.renameSync(
   path.join(projectDir, 'resources/MODULE_NAME.properties'),
   path.join(projectDir, 'resources/' + projectName + '.properties')
-);
+)
 
-// find and replace all the $$MODULE_NAME$$ and $$CAMEL_MODULE_NAME$$ markers with the appropriate project name
+// find and replace all markers with the appropriate substitution values
 const targetFiles = [
   path.join(projectDir, 'README.md'),
   path.join(projectDir, 'import.xml'),
   path.join(projectDir, 'package.json'),
   path.join(projectDir, 'definitions.cnd')
-];
+]
 
 try {
   replace.sync({
     files: targetFiles,
+    from: /\$\$YARN_PACKAGE_NAME\$\$/g,
+    to: yarnPackageName
+  })
+  replace.sync({
+    files: targetFiles,
     from: /\$\$CAMEL_MODULE_NAME\$\$/g,
-    to: camelProjectName,
-  });
+    to: camelProjectName
+  })
   replace.sync({
     files: targetFiles,
     from: /\$\$MODULE_NAME\$\$/g,
-    to: projectName,
-  });
+    to: projectName
+  })
 } catch (error) {
-  console.error('Error occurred:', error);
+  console.error('Error occurred:', error)
 }
 
-console.log('Success! Your new project is ready.');
-console.log(`Created ${projectName} at ${projectDir}`);
-console.log('You can now change into your project and launch "yarn" to install everything to get started.');
-console.log('You can also check the documentation available here for more details: https://academy.jahia.com/get-started/developers/templating');
+console.log('Success! Your new project is ready.')
+console.log(`Created ${projectName} at ${projectDir}`)
+console.log('You can now change into your project and launch "yarn" to install everything to get started.')
+console.log('You can also check the documentation available here for more details: https://academy.jahia.com/get-started/developers/templating')
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,48 +1,54 @@
-const path = require('path');
-const WebpackShellPlugin = require('./WebpackShellPlugin');
-const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
+const path = require('path')
+const WebpackShellPluginNext = require('webpack-shell-plugin-next')
+const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin')
 
 module.exports = (env, argv) => {
-    let config = {
-        entry: {
-            main: path.resolve(__dirname, 'src/index')
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist')
-        },
-        externals: {
-            '@jahia/server-helpers': 'jahiaHelpers'
-        },
-        plugins: [
-            new ExtraWatchWebpackPlugin({
-                files: [
-                    'src/**/*.hbs',
-                    'views/**/*.hbs',
-                    'images/**/*',
-                    'css/**/*',
-                    'javascript/**/*',
-                    'locales/**/*.json',
-                    'resources/**/*.properties',
-                    'definitions.cnd'
-                ]
-            }),
-        ],
-        devtool: "inline-source-map"
-    };
+  const config = {
+    entry: {
+      main: path.resolve(__dirname, 'src/index')
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist')
+    },
+    externals: {
+      '@jahia/server-helpers': 'jahiaHelpers'
+    },
+    plugins: [
+      new ExtraWatchWebpackPlugin({
+        files: [
+          'src/**/*',
+          'components/**/*',
+          'views/**/*',
+          'images/**/*',
+          'css/**/*',
+          'javascript/**/*',
+          'locales/**/*.json',
+          'resources/**/*.properties',
+          'definitions.cnd',
+          'import.xml',
+          'package.json'
+        ]
+      })
+    ],
+    devtool: 'inline-source-map'
+  }
 
-    if (env.deploy) {
-        config.plugins.push(
-            new WebpackShellPlugin({
-                onBuildEnd: ['yarn pack && yarn deploy']
-            })
-        )
-    }
-    if (env.remoteDeploy) {
-        config.plugins.push(
-            new WebpackShellPlugin({
-                onBuildEnd: ['yarn pack && yarn remoteDeploy']
-            })
-        )
-    }
-    return config;
-};
+  if (env.deploy) {
+    config.plugins.push(
+      new WebpackShellPluginNext({
+        onBuildEnd: {
+          scripts: ['yarn pack && yarn deploy']
+        }
+      })
+    )
+  } else if (env.remoteDeploy) {
+    config.plugins.push(
+      new WebpackShellPluginNext({
+        onBuildEnd: {
+          scripts: ['yarn pack && yarn remoteDeploy']
+        }
+      })
+    )
+  }
+  return config
+}
