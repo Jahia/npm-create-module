e30cda3e56d6fe99a5709cea3d08e71056b23ba4
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-12-05 14:45:09 +0100

    fix

--- jsx/webpack.client.js
+++ jsx/webpack.client.js
@@ -52,7 +52,7 @@ module.exports = env => {
         },
         plugins: [
             new ModuleFederationPlugin({
-                name: $$MODULE_NAME$$,
+                name: '$$MODULE_NAME$$',
                 library: {type: 'assign', name: 'window.appShell = (typeof appShell === "undefined" ? {} : appShell); window.appShell[\'npm-solid-react-templateset\']'},
                 filename: '../javascript/remote.js',
                 exposes: {
