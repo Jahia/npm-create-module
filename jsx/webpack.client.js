d06bbb8aadf0ab264ce2df89c2a99af973767f4a
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-12-05 10:54:06 +0100

    add part of required changes

--- jsx/webpack.client.js
+++ jsx/webpack.client.js
@@ -52,7 +52,7 @@ module.exports = env => {
         },
         plugins: [
             new ModuleFederationPlugin({
-                name: 'npm-solid-react-templateset',
+                name: 'npm-solid-react-templateset-tuto',
                 library: {type: 'assign', name: 'window.appShell = (typeof appShell === "undefined" ? {} : appShell); window.appShell[\'npm-solid-react-templateset\']'},
                 filename: '../javascript/remote.js',
                 exposes: {
