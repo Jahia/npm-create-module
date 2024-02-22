923d65b0d222f495e687037a74bf2df370ffb1d3
Serge Huber <shuber@jahia.com>
2024-02-22 16:21:49 +0100

    Remove components from registration

--- jsx/src/server/index.js
+++ jsx/src/server/index.js
@@ -1,8 +1,6 @@
-import * as jahiaComponents from './components';
 import * as jahiaTemplates from './templates';
 import * as jahiaViews from './views';
 import {registerJahiaComponents} from '@jahia/js-server-engine';
 
-registerJahiaComponents(jahiaComponents);
 registerJahiaComponents(jahiaTemplates);
 registerJahiaComponents(jahiaViews);
