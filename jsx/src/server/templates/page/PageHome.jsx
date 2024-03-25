53543fb125ff20e26fa5a048862e58daf97c55a9
Serge Huber <shuber@jahia.com>
2024-03-25 16:30:47 +0100

    BACKLOG-22506 Update to latests library changes (#28)
    * BACKLOG-22506 Update to latests library changes
    
    * Update public path to reflect latest lib changes

--- jsx/src/server/templates/page/PageHome.jsx
+++ jsx/src/server/templates/page/PageHome.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import {JArea, JAddResources} from '@jahia/js-server-engine';
+import {Area, AddResources} from '@jahia/js-server-engine';
 
 export const PageHome = () => {
     return (<>
         <head>
-            <JAddResources type='css' resources='styles.css' />
+            <AddResources type='css' resources='styles.css' />
             <title>Home</title>
         </head>
         <body>
             <h1>Home Template</h1>
             <main>
-                <JArea name="pagecontent" />
+                <Area name="pagecontent" />
             </main>
         </body>
     </>);
 }
 
 PageHome.jahiaComponent = { // This object is used to register the template in Jahia
-    id: '$$MODULE_NAMESPACE$$_templates_page_PageHome', // A globally unique identifier use to register the template
     nodeType: 'jnt:page', // The content node type the template applies to
     name: 'home', // The name of the template
     displayName: 'Home page', // The display name of the page template
