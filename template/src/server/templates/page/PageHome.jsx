c7cf60e80e34f2d1e0d11a16d0be2a318b436a86
Baptiste Grimaud <bgrimaud@jahia.com>
2024-09-30 14:50:11 +0200

    BACKLOG-23250: Configure linter + lint code from template

--- template/src/server/templates/page/PageHome.jsx
+++ template/src/server/templates/page/PageHome.jsx
@@ -6,20 +6,22 @@ export const PageHome = () => {
     const {t} = useTranslation();
     const {currentResource} = useServerContext();
     const lang = currentResource.getLocale().getLanguage();
-    return (<html lang={lang}>
-        <head>
-            <AddResources type='css' resources='styles.css' />
-            <title>Home</title>
-        </head>
-        <body>
-            <main>
-                {/* Using i18next defined in locales */}
-                <h1>{t('homeTitle')}</h1>
-                <Area name="pagecontent" />
-            </main>
-        </body>
-    </html>);
-}
+    return (
+        <html lang={lang}>
+            <head>
+                <AddResources type="css" resources="styles.css"/>
+                <title>Home</title>
+            </head>
+            <body>
+                <main>
+                    {/* Using i18next defined in locales */}
+                    <h1>{t('homeTitle')}</h1>
+                    <Area name="pagecontent"/>
+                </main>
+            </body>
+        </html>
+    );
+};
 
 PageHome.jahiaComponent = defineJahiaComponent({
     nodeType: 'jnt:page',
--- template/src/server/views/simpleContent/SimpleContentDefault.jsx
+++ template/src/server/views/simpleContent/SimpleContentDefault.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import {useServerContext, getNodeProps, defineJahiaComponent} from '@jahia/js-server-core'
+import React from 'react';
+import {useServerContext, getNodeProps, defineJahiaComponent} from '@jahia/js-server-core';
 
 export const SimpleContentDefault = () => {
-    const { currentNode } = useServerContext();
+    const {currentNode} = useServerContext();
     const simpleContent = getNodeProps(currentNode, ['title']);
     return (
         <div>
             <h2>{simpleContent.title}</h2>
         </div>
-    )
-}
+    );
+};
 
 SimpleContentDefault.jahiaComponent = defineJahiaComponent({
     name: 'default',
